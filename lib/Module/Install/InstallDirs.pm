56e0d9ed01ab8985f14319b93ab6e893e4a02ac0
Graham Barr <gbarr@pobox.com>
2009-10-21 15:34:31 +0000

    Import of GBARR/Module-Install-InstallDirs-0.02 from CPAN.
    gitpan-cpan-distribution: Module-Install-InstallDirs
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GBARR/Module-Install-InstallDirs-0.02.tar.gz
    gitpan-cpan-author:       GBARR
    gitpan-cpan-maturity:     released

--- lib/Module/Install/InstallDirs.pm
+++ lib/Module/Install/InstallDirs.pm
@@ -6,7 +6,7 @@ package Module::Install::InstallDirs;
 use Module::Install::Base; @ISA = qw(Module::Install::Base);
 use Config;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 use strict;
 
 sub installdirs {
